75477c0e956236d5378d9b8cf87a07dfee2f13ba
unknown <enioluwaarowo@gmail.com>
2021-06-12 05:54:40 +0100

    changes to the loading

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,11 @@ const par2 = document.querySelector('#par2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
-    par1.textContent('Loading...')
+    par1.textContent= 'Loading...' 
     fetch('/weather?address='+ location).then((response)=> {
     response.json().then((data) => {
         if (data.error) {
-            par1.textContent(data.error);
+            par1.textContent= data.error;
         } else {
             par1.textContent = data.location;
             par2.textContent = data.forecast;
